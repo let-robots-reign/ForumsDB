396ca109b70ea0563c68104af0afe33927503078
Алексей <zotov.alexei.1@yandex.ru>
2021-06-17 20:10:44 +0300

    index.ts and config files

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express = require("express");
+import bodyParser = require("body-parser");
+
+const app: express.Application = express();
+
+app.use(bodyParser.json());
+
+const port = 5000;
+app.listen(port,  () => {
+    console.log(`Listening on port ${port}...`);
+});
