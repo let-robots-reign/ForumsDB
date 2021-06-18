6662323ce694609c7e082b9eb3920da8568c37a5
Алексей <zotov.alexei.1@yandex.ru>
2021-06-19 00:38:23 +0300

    feature: add router to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import express = require('express');
 import bodyParser = require('body-parser');
+import router from './utils/router'
 
 const app: express.Application = express();
 
 app.use(bodyParser.json());
+app.use('/api', router);
 
 const port = 5000;
 app.listen(port,  () => {
